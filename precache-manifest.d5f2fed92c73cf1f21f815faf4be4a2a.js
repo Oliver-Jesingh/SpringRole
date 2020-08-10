f7e21f05395b9373f2904a11e00e766f73bcb829
common <oliverjesingh@gmail.com>
2020-08-10 23:44:32 +0530

    Updates

---
+++ precache-manifest.d5f2fed92c73cf1f21f815faf4be4a2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa3d172a97d0105960937b96f46f022",
+    "url": "/SpringRole/index.html"
+  },
+  {
+    "revision": "9e9f92b0cfef0ce135f2",
+    "url": "/SpringRole/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3b9a5732903dda07e696",
+    "url": "/SpringRole/static/css/main.0275df9c.chunk.css"
+  },
+  {
+    "revision": "9e9f92b0cfef0ce135f2",
+    "url": "/SpringRole/static/js/2.f3e3395a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SpringRole/static/js/2.f3e3395a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9a5732903dda07e696",
+    "url": "/SpringRole/static/js/main.8f623ec7.chunk.js"
+  },
+  {
+    "revision": "81f25abb84aeae1543b3",
+    "url": "/SpringRole/static/js/runtime-main.55f0173b.js"
+  },
+  {
+    "revision": "43505c2c7fed97b9694d41c52385043d",
+    "url": "/SpringRole/static/media/header-image.43505c2c.jpg"
+  },
+  {
+    "revision": "03b4c52b64d1276a563b39387b00d1bc",
+    "url": "/SpringRole/static/media/work1.03b4c52b.svg"
+  },
+  {
+    "revision": "c46a91d7eefc358ce7c944d4de9cf788",
+    "url": "/SpringRole/static/media/work2.c46a91d7.svg"
+  }
+]);
